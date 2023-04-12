09b73304f1aa88b7288eec35d6fb18bf7bab0e85
tenxinyun <65179334@qq.com>
2023-04-12 10:06:26 +0800

    第二次修改AutoLoginFilter去掉熊少文

--- src/main/java/cn/ybzy/filter/AutoLoginFilter.java
+++ src/main/java/cn/ybzy/filter/AutoLoginFilter.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import cn.ybzy.mvcproject.utils.CookieUtils;
-//xiongshao熊少文
+//xiongshao
 public class AutoLoginFilter extends HttpFilter {
 
 	@Override
